7a7812ee8e4b2c9b549b3b473303ea1be7d516c0
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-02-10 01:58:11 +0100

    [REFAC]: update Resend client initialization to use direct import and modify TypeScript module settings

--- src/configs/resendConfig.ts
+++ src/configs/resendConfig.ts
@@ -2,9 +2,7 @@
  * Resend email client configuration.
  * @see https://resend.com/docs/send-with-express
  */
+import { Resend } from 'resend';
 import { RESEND_API_KEY } from './envConfig';
 
-export const resend = async () => {
-  const { Resend } = await import('resend');
-  return new Resend(RESEND_API_KEY!);
-};
+export const resend = new Resend(RESEND_API_KEY!);
