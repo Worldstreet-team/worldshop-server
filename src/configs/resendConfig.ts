0589e65a80ea084ea266d26c4848df8bc3dc0a99
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-02-10 02:07:51 +0100

    [REFAC]: update build script in package.json to include npm install and refactor resendConfig for default export

--- src/configs/resendConfig.ts
+++ src/configs/resendConfig.ts
@@ -1,8 +1,7 @@
-/**
- * Resend email client configuration.
- * @see https://resend.com/docs/send-with-express
- */
+
 import { Resend } from 'resend';
 import { RESEND_API_KEY } from './envConfig';
 
-export const resend = new Resend(RESEND_API_KEY!);
+const resend = new Resend(RESEND_API_KEY);
+
+export default resend;
